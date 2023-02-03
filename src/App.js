a5f91f88e896d2a21f9f730e1ae43a44a7aa6f1a
Mydulislam <mydulislam693852@gmail.com>
2023-02-03 08:21:26 +0600

    complete internship assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Banner from './Components/Banner/Banner';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import UserDetails from './Components/Users/UserDetails';
+import Users from './Components/Users/Users';
+import Main from './layout/Main/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Main></Main>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+          path:'/',
+          element:<Banner></Banner>
+        },
+        {
+          path:'/about',
+          element:<About></About>
+        },
+        {
+          path:'/users',
+          loader:()=>fetch('https://jsonplaceholder.typicode.com/users'),
+          element:<Users></Users>
+        },
+        {
+          path:'/users/:id',
+          loader:({params})=>fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`),
+          element:<UserDetails></UserDetails>
+        },
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
